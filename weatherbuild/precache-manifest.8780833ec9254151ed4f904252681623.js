8fb0f8229a108dfb54936776daea4d65b8aad9e1
Justin Wozniak <43418633+JustinWozniak@users.noreply.github.com>
2019-04-16 00:42:31 -0400

    Add files via upload

---
+++ weatherbuild/precache-manifest.8780833ec9254151ed4f904252681623.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4015273f2d0ec5896a5512ac348e15f9",
+    "url": "/static/media/rain.4015273f.jpg"
+  },
+  {
+    "revision": "d2b6b741547383da56a4",
+    "url": "/static/css/main.aefcd382.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c8a8c1e394a1d9a5641f",
+    "url": "/static/js/2.8f1c5f7b.chunk.js"
+  },
+  {
+    "revision": "919a2eae6350d73c27b330c39ba3ed3b",
+    "url": "/static/media/Background.919a2eae.jpg"
+  },
+  {
+    "revision": "b386d54edcb329303fcb486df1cc9fdf",
+    "url": "/static/media/sunlogo.b386d54e.jpg"
+  },
+  {
+    "revision": "d2b6b741547383da56a4",
+    "url": "/static/js/main.7fc1cf4e.chunk.js"
+  },
+  {
+    "revision": "7e6b0eea85f536c25f41dde6e5ca3ca5",
+    "url": "/static/media/snow.7e6b0eea.jpg"
+  },
+  {
+    "revision": "4daf05f548e574965ab6fe0d508631ab",
+    "url": "/static/media/sun.4daf05f5.jpg"
+  },
+  {
+    "revision": "bb25671772585a383279b1e675ace231",
+    "url": "/static/media/thunder.bb256717.jpg"
+  },
+  {
+    "revision": "89aa4886dc022def5616360107a99f4e",
+    "url": "/static/media/wind.89aa4886.jpg"
+  },
+  {
+    "revision": "83e5297372d7375563e411cdda2f8659",
+    "url": "/index.html"
+  }
+];
